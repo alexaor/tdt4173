0bdbfa9aa64a6a15e17bf260a87d15d18b8b1083
Oskar Veggeland <oveggeland@gmail.com>
2020-11-01 13:22:22 +0100

    imputing works

--- preprocess/main.py
+++ preprocess/main.py
@@ -1,30 +1,47 @@
 import numpy as np
 import pandas as pd
-from tools import impute_data
 from tools import create_reduced_set
+from tools import impute_data
 
 """ CHECKLIST:
-- reduced set: check
-
-
+- Reduce set: CHECK
+- Impute missing data: CHECK
+- Encode categorical data:
+- Split data set:
+- Normalize data set:
 """
 
-
-def create_subset():
+def reduce_data_set():
     rows = np.r_[:10]
     columns = np.r_[:20, -4:0]
     create_reduced_set(rows, columns, "reduced.csv")
 
+def impute_data_set():
+    source = "reduced.csv"
+    target = "imputed.csv"
+
+    print("Imputing "+source+" to "+target)
+
+    dataset = pd.read_csv(source)
+    X = dataset.iloc[:, 1:].values
+    imputed = impute_data(X)
+    
+    df = pd.DataFrame(imputed)
+    df.to_csv(target)
+
+def encode_data_set():
+    print("hello world")
 
 def main():
-    create_subset()
-    dataset = pd.read_csv('reduced.csv')
-    X = dataset.iloc[:, :].values
-    imputed = impute_data(X)
+    #%% Creates a reduced csv file with rows, columns and filename specified in create_reduced_csv() ###
+    #reduce_data_set()
 
-    df = pd.DataFrame(imputed)
-    df.to_csv('imputed.csv')
-    
+    #%% Imputing the missing data, make sure that you change the filename if neccesary ###
+    #impute_data_set()
+
+    #%% Encode categorical data, converting each category into its own column and setting value 1 if the corresponding row belongs to that category ###
+    encode_data_set()
+
 
 if __name__ == "__main__":
     main()
