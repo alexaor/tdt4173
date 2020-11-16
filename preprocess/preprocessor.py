575adab776233654c39273984f7a0e1c34daad34
Oskar Veggeland <oveggeland@gmail.com>
2020-11-16 15:34:58 +0100

    rewrite of preprocessor

--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -4,12 +4,14 @@ import pathlib
 
 from sklearn.model_selection import train_test_split
 
+from preprocess.tools import filter_desired_features
+from preprocess.tools import impute_data
+from preprocess.tools import encode_data
 from preprocess.tools import feature_selection
-from preprocess.tools import feature_scale
-from preprocess.tools import reduce_impute_encode
+from preprocess.tools import standarize_data
 
 
-def create_data_set(filename, n_features=-1, columns=np.r_[2:6,7:12,14:21,39:45,51:56,73:90,107,-1], rows=np.r_[:8378], test_size=0.2, standarize=True):
+def create_data_set(filename, n_features=-1, columns=np.r_[2:6,7:12,14:21,39:45,51:56,73:90,107,-1], rows=np.r_[:8378], test_size=0.2, feature_scale=True):
     ### Defining dataset paths
     origin = pathlib.Path('preprocess/speeddating.csv')
     train_path = pathlib.Path('preprocess/datasets/'+filename+"_train.csv")
@@ -18,8 +20,17 @@ def create_data_set(filename, n_features=-1, columns=np.r_[2:6,7:12,14:21,39:45,
     ### Loading dataset
     Z = pd.read_csv(origin, dtype="str")
     
-    ### Pick out columns and rows, impute missing data and encode categorical data
-    Z = reduce_impute_encode(Z, rows, columns)
+    ### Remove unwanted attributes
+    Z = filter_desired_features(Z, columns)
+    
+    ### Impute missing data
+    Z = impute_data(Z)
+    
+    print(Z.columns.values)
+    print(len(Z.columns.values))
+    ### Encode categorical data
+    Z = encode_data(Z)
+    
     if n_features != -1:
         ### Choose best n_features
         Z = feature_selection(Z, n_features)
@@ -28,18 +39,9 @@ def create_data_set(filename, n_features=-1, columns=np.r_[2:6,7:12,14:21,39:45,
     Z_train, Z_test = train_test_split(Z, test_size=test_size)
 
     ### Feature scaling by means of standarization
-    if standarize:
-        Z_train, Z_test = feature_scale(Z_train, Z_test)
+    if feature_scale:
+        Z_train, Z_test = standarize_data(Z_train, Z_test)
         
     ### Save processed dataset
     Z_train.to_csv(train_path, index=False)
     Z_test.to_csv(test_path, index=False)
-
-
-def main():
-    create_data_set("full_set", rows=np.r_[:10])
-
-
-if __name__ == "__main__":
-    main()
-
--- preprocess/tools.py
+++ preprocess/tools.py
@@ -8,22 +8,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectKBest
 
 
-def reduce_impute_encode(df, rows, columns):
-    ## Import and reduce dataset ##
-    Z = df.iloc[rows, columns].values
+def filter_desired_features(df, columns):
+    Z = df.iloc[:, columns].values
     column_labels = df.columns.values[columns]
-
+    
+    df = pd.DataFrame(Z, columns = column_labels)
+    return df
+    
+def impute_data(df):
+    ## Import and reduce dataset ##
+    Z = df.iloc[:, :].values
+    column_labels = df.columns.values
+    
     ## Impute dataset ##
     imputer = SimpleImputer(missing_values='?', strategy='most_frequent')
     imputer.fit(Z)
     Z_imputed = imputer.transform(Z)
-    df = pd.DataFrame(Z_imputed, columns = column_labels)
     
-    ## Encoding data ##    
-    df = encode_data(df)
-    return df.astype(np.float)
-
-
+    df = pd.DataFrame(Z_imputed, columns = column_labels)
+    return df
+    
 """
 @param data_row: One dimensional array with eather categorical or non-categorical data
 """
@@ -65,43 +69,7 @@ def encode_data(dataset):
     column_names = np.concatenate((cat_col_names, column_names))
     
     df = pd.DataFrame(X, columns = column_names)
-    return df
-
-
-"""
-@param X: Independent variable array
-@param Y: Dependent variable vector
-@param size: Size of test set (between 0 and 1)
-@param seed: Optional seed for reproducing results
-"""
-def split_set(X, size = 0.2, seed = None):
-    return train_test_split(X, test_size = size, random_state = seed) 
-
-
-
-"""
-@param X_train: Array of training data
-@param X_test: Array of test data
-"""
-def feature_scale(Z_train, Z_test):
-    X_train = Z_train.iloc[:, :-1].values
-    y_train = Z_train.iloc[:, -1].values
-    X_test = Z_test.iloc[:, :-1].values
-    y_test = Z_test.iloc[:, -1].values
-    
-    y_train = y_train.reshape(y_train.shape[0], 1)
-    y_test = y_test.reshape(y_test.shape[0], 1)
-    
-    sc = StandardScaler()
-    X_train_scaled = sc.fit_transform(X_train)
-    X_test_scaled = sc.transform(X_test)
-
-    cols = Z_train.columns.values
-    df_train = pd.DataFrame(np.hstack((X_train_scaled, y_train)), columns = cols)
-    df_test = pd.DataFrame(np.hstack((X_test_scaled, y_test)), columns = cols)
-    
-    return df_train, df_test
-
+    return df.astype(np.float)
 
 
 def feature_selection(Z, n_features):
@@ -124,3 +92,27 @@ def feature_selection(Z, n_features):
 
     return df
     
+
+
+"""
+@param X_train: Array of training data
+@param X_test: Array of test data
+"""
+def standarize_data(Z_train, Z_test):
+    X_train = Z_train.iloc[:, :-1].values
+    y_train = Z_train.iloc[:, -1].values
+    X_test = Z_test.iloc[:, :-1].values
+    y_test = Z_test.iloc[:, -1].values
+    
+    y_train = y_train.reshape(y_train.shape[0], 1)
+    y_test = y_test.reshape(y_test.shape[0], 1)
+    
+    sc = StandardScaler()
+    X_train_scaled = sc.fit_transform(X_train)
+    X_test_scaled = sc.transform(X_test)
+
+    cols = Z_train.columns.values
+    df_train = pd.DataFrame(np.hstack((X_train_scaled, y_train)), columns = cols)
+    df_test = pd.DataFrame(np.hstack((X_test_scaled, y_test)), columns = cols)
+    
+    return df_train, df_test
