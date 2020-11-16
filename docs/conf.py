4f7a534c7365482a5383687e5d090508f78d6f2a
Alexander Orvik <alexaor@stud.ntnu.no>
2020-11-16 11:47:06 +0100

    add rtd theme and autodoc plugin sphinx
    Add readthedocs plugin theme for styling
    Add autodoc module for autodocumentation
    Bootstrap initial doc steructure with content/ folder

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +33,7 @@ release = '26-11-2020'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc'
+        'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,10 @@
-.. TDT4173 - Machine Learning Project documentation master file, created by
-   sphinx-quickstart on Sun Nov 15 17:50:02 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to TDT4173 - Machine Learning Project's documentation!
 ==============================================================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :titlesonly:
 
+   content/classifier
+   content/models
+   content/util
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
